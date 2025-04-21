8cf7b040b95e4bb09639ff0d9a2f34bee03c5a78
Syam Son Penumala <syamson132000@gmail.com>
2025-04-21 21:31:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,47 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name,age){
+		this._name=name;
+		this._age=age;
+	}
+	get name(){
+		return this._name;
+	}
+	get age(){
+		return this._age;
+	}
+	set age(value) {
+    if (value >= 0) {
+      this._age = value;
+    } else {
+      console.error("Age must be a positive number.");
+    }
+  }
+}
 
-class Student extends Person {}
+class Student extends Person {	
+  study() {
+    console.log(`${this.name} is studying`);
+}
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	teach() {
+    console.log(`${this.name} is teaching`);
+  }
+const person = new Person("John", 25);
+console.log(person.name); 
 
+person.age = 30;
+console.log(person.age);  
+
+// 2. Student instance
+const student = new Student("Alice", 22);
+student.study();     
+
+// 3. Teacher instance
+const teacher = new Teacher("Bob", 40);
+teacher.teach();           
 // Do not change the code below this line
 window.Person = Person;
 window.Student = Student;
